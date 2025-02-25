69e647e8f0c74ce2c1b30505f4a15edea657b854
Laurence Liang <laurence.w.liang@gmail.com>
2025-02-25 16:27:05 -0500

    Create working initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### Setup
+
+```
+pip install uv
+uv sync
+uv run pip install -e .
+```
+
+### Unit Test
+```sh
+uv run pytest
+```
---
+++ src/torchcir/cnn.py
@@ -0,0 +1,8 @@
+import torch
+
+class CNN(torch.nn.module):
+    def __init__(self):
+        super().__init__(self)
+
+    def forward(x):
+        return x
